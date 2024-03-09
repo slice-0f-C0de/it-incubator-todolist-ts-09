7ec62324a8652b3a75bbf065bf8016187ed4dcef
slice of code <slice_of_code@mail.ru>
2024-03-09 17:57:38 +0400

    addTaskAC

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -1,4 +1,5 @@
 import {TasksStateType} from "../App";
+import {v1} from "uuid";
 
 export type RemoveTaskActionType = {
     type: 'REMOVE-TASK'
@@ -6,14 +7,22 @@ export type RemoveTaskActionType = {
     todolistId: string
 }
 
-type ActionsType = RemoveTaskActionType
+export type AddTaskActionType = {
+    type: 'ADD-TASK'
+    title: string
+    todolistId: string
+}
+
+type ActionsType = RemoveTaskActionType | AddTaskActionType
 
 export const tasksReducer = (state: TasksStateType, action: ActionsType) => {
     switch (action.type) {
         case 'REMOVE-TASK':
             return {...state, [action.todolistId]:
                     state[action.todolistId].filter(task => task.id !== action.id)}
-
+        case 'ADD-TASK':
+            return {...state, [action.todolistId]:
+            [{id: v1(), title: action.title, isDone: false}, ...state[action.todolistId]]}
         default:
             throw new Error("I don't understand this type")
     }
@@ -22,4 +31,9 @@ export const tasksReducer = (state: TasksStateType, action: ActionsType) => {
 export const removeTaskAC = (id: string, todolistId: string): RemoveTaskActionType => {
     return { type: 'REMOVE-TASK', id, todolistId}
 }
+
+export const addTaskAC = (title: string, todolistId: string): AddTaskActionType => {
+    return { type: 'ADD-TASK', title, todolistId}
+}
+
 
